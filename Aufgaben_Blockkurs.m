0b055cd9d97f44152191e47fc7547a3ad0553f97
Arne Hansen <arne96.hansen@gmail.com>
2025-02-11 16:40:58 +0100

    tutorial 1 test ready

---
+++ Aufgaben_Blockkurs.m
@@ -0,0 +1,5 @@
+%% Aufgaben
+
+%   Einige Felder werden als Aufgabe gekennzeichnet sein. 
+% Bitte stellt sicher, dass ihr all diese Felder ausgefüllt habt, 
+% bevor ihr das Skript an Arne (arne.hansen@psychologie.uzh.ch) sendet.
