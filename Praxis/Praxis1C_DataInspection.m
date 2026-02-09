f5db37f42211d1eb5bd51824a133646d1557c744
Arne Hansen <arne96.hansen@gmail.com>
2026-02-09 12:53:41 +0100

    Update Praxis1C_DataInspection.m

--- Praxis/Praxis1C_DataInspection.m
+++ Praxis/Praxis1C_DataInspection.m
@@ -19,8 +19,9 @@ clc
 which hilbert 
 which gethmmalignment
 
-% Oder voller Überblick, inklusiver Version
+% Oder voller Überblick, inklusive Version
 ver
+
 %% Pfade setzen
 
 % Pfad zu den Daten
@@ -44,68 +45,62 @@ load( [pathToData  '/gip_sub-002.mat'] );
 % EEGLab-Funktion um die Positionen der Elektroden zu visualisieren
 pop_chanedit(EEG)
 
-%% Zeitverlauf der EEGd-Daten visualisieren
+%% Zeitverlauf der EEG-Daten visualisieren
 
-% Erstens : EEGLab Fuktion verwenden um gesamtes Signal zu betrachten
+% Erstens : EEGLab Funktion verwenden um gesamtes Signal zu betrachten
 % -----------------------------------------------------------------------
 eegplot(EEG.data)
 
-
-%Zweitens: Einzelne Kanäle visualisieren (ohne EEGLab)
+% Zweitens: Einzelne Kanäle visualisieren (ohne EEGLab)
 % -----------------------------------------------------------------------
 
 % -------------
 % Zuerst: Wiederholung Indizierung am Beispiel der EEG Daten
 size(EEG.data) % Matrix mit zwei Dimensionen 
 
-%Aufgabe: Auf Datenpunkt 100 von Kanal 30 zugreifen:
+% Aufgabe: Auf Datenpunkt 100 von Kanal 30 zugreifen:
 EEG.data(30,100)
 % -------------
-
 
 % Jetzt:  die ersten 500 Datenpunkte von Kanal 1 und Kanal 50 plotten
 nChannelA = 1;
 nChannelB = 50;
 
 minPnt = 1;
-maxPnt = 500; %Wieviel Zeit vergeht von minPoint zu MaxPoint?
+maxPnt = 500; % Wie viel Zeit vergeht von minPnt zu maxPnt?
 
-%Die folgene Abbildung soll zuerst das EEG Signal von channelA, von
-%minPnt bis maxPnt plotten, und danach noch das EEG Signal von 
+% Die folgende Abbildung soll zuerst das EEG Signal von channelA, von
+% minPnt bis maxPnt plotten, und danach noch das EEG Signal von 
 % Auf der x-Achse soll die Zeit in ms aufgetragen werden, auf der y-Achse
 % die Ausprägung des EEG Signals
 
-%leere Abbildung öffnen:
+% leere Abbildung öffnen:
 figure;
-%Daten von channelA plotten:
+% Daten von channelA plotten:
 plot(EEG.times( minPnt:maxPnt ) , EEG.data( nChannelA , minPnt:maxPnt ),'color','r')
 hold on % erlaubt das plotten mehrerer Signale in einer Abbildung
-%Daten von channelB plotten:
+% Daten von channelB plotten:
 plot(EEG.times( minPnt:maxPnt ),EEG.data( nChannelB , minPnt:maxPnt),'color','y')
 legend('Kanal A','Kanal B')
 
-%Titel & Achsenbeschriftungen hinzufügen:
+% Titel & Achsenbeschriftungen hinzufügen:
 title(['Timecourse of two EEG channels']);
 xlabel('time [ms]')
 ylabel('signal [uV]')
 
-
-
-
 % Dieser Befehl schliesst alle Figuren die geöffnet waren
 close all
 
-%% Topographien plotten & Rerefernzierung
+%% Topographien plotten & Rereferenzierung
 
 % Wir wählen einen arbiträren Datenpunkt (500).
-% Nach wievielen ms wurde dieser Datenpunkt aufgenommen? 
+% Nach wie vielen ms wurde dieser Datenpunkt aufgenommen? 
 
 % (Datenpunkt 1 korrespondiert zu Millisekunde 0, und wir haben eine
 % Abtastrate von 250 samples/sek)
-
 timePnt = 500;
 
-% Wir wollen die funktion "topoplot" benutzen, also sehen wir uns an wie
+% Wir wollen die Funktion "topoplot" benutzen, also sehen wir uns an wie
 % diese verwendet werden soll:
 help topoplot
 
@@ -113,7 +108,7 @@ help topoplot
 chlLeftEar = 13;
 chlRightForehead = 8;
 
-%Abbildung öffnen:
+% Abbildung öffnen:
 figure; % wir werden subplots verwenden - siehe whiteboard!
 
 % Erster Plot, Daten mit bestehender Referenz plotten
@@ -150,8 +145,10 @@ colorbar
 title('Data rereferenced to average')
 
 
-%% Uebung: Titel der Plots verbessern, sodass sie beim 3. und 4. plot auch 
-% die Nummer der Elektrode ausgeben die in der variable chlLeftEar bzw 
+%% Übung: Titel der Plots verbessern, sodass sie beim 3. und 4. Plot auch 
+% die Nummer der Elektrode ausgeben die in der Variable chlLeftEar bzw 
 % chlRightForehead anzeigen, also zum Beispiel:
 % "Data rereferenced near left ear, electrode number: 13"
 % "Hardcoding" ist nicht erlaubt :)
+
+%% FREIWILLIGE Hausaufgabe
